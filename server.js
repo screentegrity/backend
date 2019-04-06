31e4afcadff736e75cb5e7dc379a841cd94ca9e8
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-06 14:22:43 -0700

    Converted to use apollo-server instead of apollo-server-express

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
-const http = require("http");
-const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
+//const http = require("http");
+//const express = require("express");
+//const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer, gql } = require("apollo-server");
 const { resolvers } = require("./resolvers");
 
 const typeDefs = gql(
   fs.readFileSync(__dirname.concat("/schema/schema.graphql"), "utf8")
 );
 const PORT = process.env.PORT || 3000;
-const app = express();
+// const app = express();
 
 const apolloServer = new ApolloServer({ 
   typeDefs, 
@@ -16,7 +17,7 @@ const apolloServer = new ApolloServer({
   context: async ({ req, connection }) => {
     if (connection) {
       // check connection for metadata
-      return {};
+      return connection.context;
     } else {
       // check from req
       const token = req.headers.authorization || "";
@@ -26,19 +27,24 @@ const apolloServer = new ApolloServer({
   },
 });
 
-apolloServer.applyMiddleware({ app });
+// apolloServer.applyMiddleware({ app });
 
-const httpServer = http.createServer(app);
-apolloServer.installSubscriptionHandlers(httpServer);
+// const httpServer = http.createServer(app);
+// apolloServer.installSubscriptionHandlers(httpServer);
 
 // ⚠️ Pay attention to the fact that we are calling `listen` on the http server variable, and not on `app`.
-httpServer.listen(PORT, () => {
-  console.log(
-    `🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`
-  );
-  console.log(
-    `🚀 Subscriptions ready at ws://localhost:${PORT}${
-      apolloServer.subscriptionsPath
-    }`
-  );
+// httpServer.listen(PORT, () => {
+//   console.log(
+//     `🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`
+//   );
+//   console.log(
+//     `🚀 Subscriptions ready at ws://localhost:${PORT}${
+//       apolloServer.subscriptionsPath
+//     }`
+//   );
+// });
+
+apolloServer.listen(PORT).then(({ url, subscriptionsUrl }) => {
+  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
 });
